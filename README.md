e292b667962e804eda4d2405ee701cbdb29e49b3
Shinji Ogaki <shinji.ogaki@gmail.com>
2021-02-08 21:11:59 +0900

    add code path for small jitter(<= 0.5)

--- README.md
+++ README.md
@@ -2,18 +2,38 @@
 <img src="img/stone.jpg" alt="stone" title="stone" width="320"><img src="img/flake.png" alt="flake" title="flake" width="320">
 
 ## What is this?
+
 **voroce** is a fast and simple voronoi class useful to create images like the above ones.
 **voroce** is a coind word of **voronoi** & **veloce**.
 
+## Introduction
+
+<img src="img/figure3.png" alt="order" title="order" width="320">
+
+[Voronoi](http://www.rhythmiccanvas.com/research/papers/worley.pdf) is very useful for generating interesting patterns. Surprisingly, however, I have not found much literature on how to compute them efficiently. The most relevant work to this project was done by [Jontier et al.](http://jcgt.org/published/0008/01/02/paper.pdf) who proposed an optimal visiting order for efficient cellular noise generation. In the 2D case, they describe the order of 20 neighboring cells, but I think it is not optimal because some cells do not need to be visited (let me know if I'm wrong :-)). If the shading point is in the green region and each cell has at least one sample point, it is not necessary to visit the cells 10, 13, 14, 15, 16, 17, 18, and 19. (For example, the cell 10 cannot have a sample point closer than the one in the cell 0.) In [voronoise](https://iquilezles.org/www/articles/voronoise/voronoise.htm) by Inigo Quilez, sample points are jittered so that cell noise and voronoi can be generated in a single framework. The number of neighboring cells to be visited depends on the amount of jitter.
+
+|      |  naive implementation | optimized (0.5 < jitter) | optimized (0.5 >= jitter) |
+| ---- | --------------------: | -----------------------: | ------------------------: |
+|  2d  |                    21 |                       13 |                         4 |
+|  3d  |                   117 |                       39 |                        20 |
+|  4d  |                   609 |                      195 |            fewer than 100 |
+
+## Features
+
+* sample point jittering (grid <-> voronoi)
+* optimized code path for small jitter values
+
 I have a couple of ideas to speed up. So stay tuned!
 
-## Cost Comparison (relative to naive 2d implementation)
+## Cost Comparison
 
-|      |  naive impl |  optimized |
-| ---- | ----------: | ---------: |
-|  2d  |        1.00 |       0.48 |
-|  3d  |        6.48 |       1.18 |
-|  4d  |       48.86 |      10.49 |
+### Time (relative to naive 2d implementation)
+
+|      |  naive implementation | optimized (0.5 < jitter) | optimized (0.5 >= jitter) |
+| ---- | --------------------: | -----------------------: | ------------------------: |
+|  2d  |                  1.00 |                     0.61 |                      0.33 |
+|  3d  |                  6.37 |                     1.23 |                      0.83 |
+|  4d  |                 44.76 |                     8.79 |                      4.08 |
 
 ## Dependencies
 * [glm](https://github.com/g-truc/glm)
--- test/main.cpp
+++ test/main.cpp
@@ -16,17 +16,20 @@ int main()
 	const auto N = 1000000;
 	const auto T = 10000000;
 
+	////////////////////////////////////////////////////////////////
+	// 2D
+	////////////////////////////////////////////////////////////////
 	{
 		const auto start = std::chrono::system_clock::now();
 		for (auto i = 0; i < N; ++i)
 		{
 			const auto x = (i + 0.5) / N;
 			const auto y = (i * golden_ratio) - std::floor(i * golden_ratio);
-			volatile auto v = Voroce::Evaluate2DRef(glm::vec2(x * 128 - 64, y * 128 - 64), Voroce::Hash2DLowQuality).first;
+			volatile auto v = Voroce::Evaluate2DRef(glm::vec2(x * 2048 - 1024, y * 2048 - 1024), Voroce::Hash2DLowQuality, 0.5).first;
 		}
 		const auto end = std::chrono::system_clock::now();
 		const auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-		std::cout << "2d ref: " << elapsed << std::endl;
+		std::cout << "2d ref half: " << elapsed << std::endl;
 	}
 
 	{
@@ -35,11 +38,37 @@ int main()
 		{
 			const auto x = (i + 0.5) / N;
 			const auto y = (i * golden_ratio) - std::floor(i * golden_ratio);
-			volatile auto v = Voroce::Evaluate2DOpt(glm::vec2(x * 128 - 64, y * 128 - 64), Voroce::Hash2DLowQuality).first;
+			volatile auto v = Voroce::Evaluate2DOpt(glm::vec2(x * 2048 - 1024, y * 2048 - 1024), Voroce::Hash2DLowQuality, 0.5f).first;
 		}
 		const auto end = std::chrono::system_clock::now();
 		const auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-		std::cout << "2d opt: " << elapsed << std::endl;
+		std::cout << "2d opt half: " << elapsed << std::endl;
+	}
+
+	{
+		const auto start = std::chrono::system_clock::now();
+		for (auto i = 0; i < N; ++i)
+		{
+			const auto x = (i + 0.5) / N;
+			const auto y = (i * golden_ratio) - std::floor(i * golden_ratio);
+			volatile auto v = Voroce::Evaluate2DRef(glm::vec2(x * 2048 - 1024, y * 2048 - 1024), Voroce::Hash2DLowQuality).first;
+		}
+		const auto end = std::chrono::system_clock::now();
+		const auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+		std::cout << "2d ref full: " << elapsed << std::endl;
+	}
+
+	{
+		const auto start = std::chrono::system_clock::now();
+		for (auto i = 0; i < N; ++i)
+		{
+			const auto x = (i + 0.5) / N;
+			const auto y = (i * golden_ratio) - std::floor(i * golden_ratio);
+			volatile auto v = Voroce::Evaluate2DOpt(glm::vec2(x * 2048 - 1024, y * 2048 - 1024), Voroce::Hash2DLowQuality).first;
+		}
+		const auto end = std::chrono::system_clock::now();
+		const auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+		std::cout << "2d opt full: " << elapsed << std::endl;
 	}
 
 	{
@@ -48,8 +77,8 @@ int main()
 		{
 			const auto x = (i + 0.5) / N;
 			const auto y = (i * golden_ratio) - std::floor(i * golden_ratio);
-			const auto ref = Voroce::Evaluate2DRef(glm::vec2(x * 128 - 64, y * 128 - 64), Voroce::Hash2DLowQuality).first;
-			const auto opt = Voroce::Evaluate2DOpt(glm::vec2(x * 128 - 64, y * 128 - 64), Voroce::Hash2DLowQuality).first;
+			const auto ref = Voroce::Evaluate2DRef(glm::vec2(x * 2048 - 1024, y * 2048 - 1024), Voroce::Hash2DLowQuality, 0.5f).first;
+			const auto opt = Voroce::Evaluate2DOpt(glm::vec2(x * 2048 - 1024, y * 2048 - 1024), Voroce::Hash2DLowQuality, 0.5f).first;
 			if (ref != opt)
 			{
 				std::cout << "NG: (" << x << "," << y << ") ref:" << ref << " opt:" << opt << std::endl;
@@ -58,67 +87,10 @@ int main()
 	}
 
 	{
-		const auto start = std::chrono::system_clock::now();
-		for (auto i = 0; i < N; ++i)
-		{
-			const auto x = (i + 0.5) / N;
-			const auto y = (i * golden_ratio) - std::floor(i * golden_ratio);
-			const auto z = y;
-			volatile auto v = Voroce::Evaluate3DRef(glm::vec3(x * 128 - 64, y * 128 - 64, z * 128 - 64), Voroce::Hash3DLowQuality).first;
-		}
-		const auto end = std::chrono::system_clock::now();
-		const auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-		std::cout << "3d ref: " << elapsed << std::endl;
-	}
-
-	{
-		const auto start = std::chrono::system_clock::now();
-		for (auto i = 0; i < N; ++i)
-		{
-			const auto x = (i + 0.5) / N;
-			const auto y = (i * golden_ratio) - std::floor(i * golden_ratio);
-			const auto z = y;
-			volatile auto v = Voroce::Evaluate3DOpt(glm::vec3(x * 128 - 64, y * 128 - 64, z * 128 - 64), Voroce::Hash3DLowQuality).first;
-		}
-		const auto end = std::chrono::system_clock::now();
-		const auto  elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-		std::cout << "3d opt: " << elapsed << std::endl;
-	}
-
-	{
-		const auto start = std::chrono::system_clock::now();
-		for (auto i = 0; i < N; ++i)
-		{
-			const auto x = (i + 0.5) / N;
-			const auto y = (i * golden_ratio) - std::floor(i * golden_ratio);
-			const auto z = y;
-			const auto t = x;
-			volatile auto v = Voroce::Evaluate4DRef(glm::vec4(x * 128 - 64, y * 128 - 64, z * 128 - 64, t * 128 - 64), Voroce::Hash4DLowQuality).first;
-		}
-		const auto end = std::chrono::system_clock::now();
-		const auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-		std::cout << "4d ref: " << elapsed << std::endl;
-	}
-
-	{
-		const auto start = std::chrono::system_clock::now();
-		for (auto i = 0; i < N; ++i)
-		{
-			const auto x = (i + 0.5) / N;
-			const auto y = (i * golden_ratio) - std::floor(i * golden_ratio);
-			const auto z = y;
-			const auto t = x;
-			volatile auto v = Voroce::Evaluate4DOpt(glm::vec4(x * 128 - 64, y * 128 - 64, z * 128 - 64, t * 128 - 64), Voroce::Hash4DLowQuality).first;
-		}
-		const auto end = std::chrono::system_clock::now();
-		const auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-		std::cout << "4d opt: " << elapsed << std::endl;
-	}
-
-	{
+		// bug or numerical error
 		std::mt19937 mt(15);
 		std::uniform_real_distribution<double> uni(0.0f, 1.0f);
-		for (auto L = 0; L < 32; ++L)
+		for (auto L = 0; L < 256; ++L)
 		{
 			std::cout << "2d: " << L << std::endl;
 			std::vector<std::tuple<double, double>> RND(T);
@@ -129,21 +101,105 @@ int main()
 			std::for_each(std::execution::par, RND.begin(), RND.end(), [&](std::tuple<double, double>& tuple)
 				{
 					const auto [x, y] = tuple;
-					const auto jitter = 1.0f;
-					const auto ref = Voroce::Evaluate2DRef(glm::vec2(x * 128 - 64, y * 128 - 64), Voroce::Hash2DLowQuality, jitter);
-					const auto opt = Voroce::Evaluate2DOpt(glm::vec2(x * 128 - 64, y * 128 - 64), Voroce::Hash2DLowQuality, jitter);
+					const auto ref = Voroce::Evaluate2DRef(glm::vec2(x * 2048 - 1024, y * 2048 - 1024), Voroce::Hash2DLowQuality, 0.5f);
+					const auto opt = Voroce::Evaluate2DOpt(glm::vec2(x * 2048 - 1024, y * 2048 - 1024), Voroce::Hash2DLowQuality, 0.5f);
 					if (ref.second != opt.second)
 					{
-						std::cout << "NG: ref:" << ref.first << " opt:" << opt.first << " ref:" << std::setprecision(16) << ref.second << " opt:" << opt.second << " jitter:" << jitter << std::endl;
+						std::cout << "2d: ref:" << ref.first << " opt:" << opt.first << " ref:" << std::setprecision(16) << ref.second << " opt:" << opt.second << std::endl;
 					}
 				});
 		}
 	}
 
 	{
+		// bug or numerical error
 		std::mt19937 mt(15);
 		std::uniform_real_distribution<double> uni(0.0f, 1.0f);
-		for (auto L = 0; L < 32; ++L)
+		for (auto L = 0; L < 256; ++L)
+		{
+			std::cout << "2d: " << L << std::endl;
+			std::vector<std::tuple<double, double>> RND(T);
+			for (auto& r : RND)
+			{
+				r = { uni(mt), uni(mt) };
+			}
+			std::for_each(std::execution::par, RND.begin(), RND.end(), [&](std::tuple<double, double>& tuple)
+				{
+					const auto [x, y] = tuple;
+					const auto ref = Voroce::Evaluate2DRef(glm::vec2(x * 2048 - 1024, y * 2048 - 1024), Voroce::Hash2DLowQuality);
+					const auto opt = Voroce::Evaluate2DOpt(glm::vec2(x * 2048 - 1024, y * 2048 - 1024), Voroce::Hash2DLowQuality);
+					if (ref.second != opt.second)
+					{
+						std::cout << "2d: ref:" << ref.first << " opt:" << opt.first << " ref:" << std::setprecision(16) << ref.second << " opt:" << opt.second << std::endl;
+					}
+				});
+		}
+	}
+
+	////////////////////////////////////////////////////////////////
+	// 3D
+	////////////////////////////////////////////////////////////////
+	{
+		const auto start = std::chrono::system_clock::now();
+		for (auto i = 0; i < N; ++i)
+		{
+			const auto x = (i + 0.5) / N;
+			const auto y = (i * golden_ratio) - std::floor(i * golden_ratio);
+			const auto z = y;
+			volatile auto v = Voroce::Evaluate3DRef(glm::vec3(x * 2048 - 1024, y * 2048 - 1024, z * 2048 - 1024), Voroce::Hash3DLowQuality, 0.5f).first;
+		}
+		const auto end = std::chrono::system_clock::now();
+		const auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+		std::cout << "3d ref half: " << elapsed << std::endl;
+	}
+
+	{
+		const auto start = std::chrono::system_clock::now();
+		for (auto i = 0; i < N; ++i)
+		{
+			const auto x = (i + 0.5) / N;
+			const auto y = (i * golden_ratio) - std::floor(i * golden_ratio);
+			const auto z = y;
+			volatile auto v = Voroce::Evaluate3DOpt(glm::vec3(x * 2048 - 1024, y * 2048 - 1024, z * 2048 - 1024), Voroce::Hash3DLowQuality, 0.5f).first;
+		}
+		const auto end = std::chrono::system_clock::now();
+		const auto  elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+		std::cout << "3d opt half: " << elapsed << std::endl;
+	}
+
+	{
+		const auto start = std::chrono::system_clock::now();
+		for (auto i = 0; i < N; ++i)
+		{
+			const auto x = (i + 0.5) / N;
+			const auto y = (i * golden_ratio) - std::floor(i * golden_ratio);
+			const auto z = y;
+			volatile auto v = Voroce::Evaluate3DRef(glm::vec3(x * 2048 - 1024, y * 2048 - 1024, z * 2048 - 1024), Voroce::Hash3DLowQuality).first;
+		}
+		const auto end = std::chrono::system_clock::now();
+		const auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+		std::cout << "3d ref full: " << elapsed << std::endl;
+	}
+
+	{
+		const auto start = std::chrono::system_clock::now();
+		for (auto i = 0; i < N; ++i)
+		{
+			const auto x = (i + 0.5) / N;
+			const auto y = (i * golden_ratio) - std::floor(i * golden_ratio);
+			const auto z = y;
+			volatile auto v = Voroce::Evaluate3DOpt(glm::vec3(x * 2048 - 1024, y * 2048 - 1024, z * 2048 - 1024), Voroce::Hash3DLowQuality).first;
+		}
+		const auto end = std::chrono::system_clock::now();
+		const auto  elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+		std::cout << "3d opt full: " << elapsed << std::endl;
+	}
+
+	{
+		// bug or numerical error
+		std::mt19937 mt(15);
+		std::uniform_real_distribution<double> uni(0.0f, 1.0f);
+		for (auto L = 0; L < 256; ++L)
 		{
 			std::cout << "3d: " << L << std::endl;
 			std::vector<std::tuple<double, double, double>> RND(T);
@@ -154,12 +210,11 @@ int main()
 			std::for_each(std::execution::par, RND.begin(), RND.end(), [&](std::tuple<double, double, double>& tuple)
 				{
 					const auto [x, y, z] = tuple;
-					const auto jitter = 1.0f;
-					const auto ref = Voroce::Evaluate3DRef(glm::vec3(x * 128 - 64, y * 128 - 64, z * 128 - 64), Voroce::Hash3DLowQuality, jitter);
-					const auto opt = Voroce::Evaluate3DOpt(glm::vec3(x * 128 - 64, y * 128 - 64, z * 128 - 64), Voroce::Hash3DLowQuality, jitter);
+					const auto ref = Voroce::Evaluate3DRef(glm::vec3(x * 2048 - 1024, y * 2048 - 1024, z * 2048 - 1024), Voroce::Hash3DLowQuality, 0.5f);
+					const auto opt = Voroce::Evaluate3DOpt(glm::vec3(x * 2048 - 1024, y * 2048 - 1024, z * 2048 - 1024), Voroce::Hash3DLowQuality, 0.5f);
 					if (ref.second != opt.second)
 					{
-						std::cout << "NG: ref:" << ref.first << " opt:" << opt.first << " ref:" << std::setprecision(16) << ref.second << " opt:" << opt.second << " jitter:" << jitter << std::endl;
+						std::cout << "3d: ref:" << ref.first << " opt:" << opt.first << " ref:" << std::setprecision(16) << ref.second << " opt:" << opt.second << std::endl;
 					}
 				});
 		}
@@ -169,7 +224,95 @@ int main()
 		// bug or numerical error
 		std::mt19937 mt(15);
 		std::uniform_real_distribution<double> uni(0.0f, 1.0f);
-		for (auto L = 0; L < 32; ++L)
+		for (auto L = 0; L < 256; ++L)
+		{
+			std::cout << "3d: " << L << std::endl;
+			std::vector<std::tuple<double, double, double>> RND(T);
+			for (auto& r : RND)
+			{
+				r = { uni(mt), uni(mt), uni(mt) };
+			}
+			std::for_each(std::execution::par, RND.begin(), RND.end(), [&](std::tuple<double, double, double>& tuple)
+				{
+					const auto [x, y, z] = tuple;
+					const auto ref = Voroce::Evaluate3DRef(glm::vec3(x * 2048 - 1024, y * 2048 - 1024, z * 2048 - 1024), Voroce::Hash3DLowQuality);
+					const auto opt = Voroce::Evaluate3DOpt(glm::vec3(x * 2048 - 1024, y * 2048 - 1024, z * 2048 - 1024), Voroce::Hash3DLowQuality);
+					if (ref.second != opt.second)
+					{
+						std::cout << "3d: ref:" << ref.first << " opt:" << opt.first << " ref:" << std::setprecision(16) << ref.second << " opt:" << opt.second << std::endl;
+					}
+				});
+		}
+	}
+
+	////////////////////////////////////////////////////////////////
+	// 4D
+	////////////////////////////////////////////////////////////////
+	{
+		const auto start = std::chrono::system_clock::now();
+		for (auto i = 0; i < N; ++i)
+		{
+			const auto x = (i + 0.5) / N;
+			const auto y = (i * golden_ratio) - std::floor(i * golden_ratio);
+			const auto z = y;
+			const auto t = x;
+			volatile auto v = Voroce::Evaluate4DRef(glm::vec4(x * 2048 - 1024, y * 2048 - 1024, z * 2048 - 1024, t * 2048 - 1024), Voroce::Hash4DLowQuality, 0.5f).first;
+		}
+		const auto end = std::chrono::system_clock::now();
+		const auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+		std::cout << "4d ref half: " << elapsed << std::endl;
+	}
+
+	{
+		const auto start = std::chrono::system_clock::now();
+		for (auto i = 0; i < N; ++i)
+		{
+			const auto x = (i + 0.5) / N;
+			const auto y = (i * golden_ratio) - std::floor(i * golden_ratio);
+			const auto z = y;
+			const auto t = x;
+			volatile auto v = Voroce::Evaluate4DOpt(glm::vec4(x * 2048 - 1024, y * 2048 - 1024, z * 2048 - 1024, t * 2048 - 1024), Voroce::Hash4DLowQuality, 0.5f).first;
+		}
+		const auto end = std::chrono::system_clock::now();
+		const auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+		std::cout << "4d opt half: " << elapsed << std::endl;
+	}
+
+	{
+		const auto start = std::chrono::system_clock::now();
+		for (auto i = 0; i < N; ++i)
+		{
+			const auto x = (i + 0.5) / N;
+			const auto y = (i * golden_ratio) - std::floor(i * golden_ratio);
+			const auto z = y;
+			const auto t = x;
+			volatile auto v = Voroce::Evaluate4DRef(glm::vec4(x * 2048 - 1024, y * 2048 - 1024, z * 2048 - 1024, t * 2048 - 1024), Voroce::Hash4DLowQuality).first;
+		}
+		const auto end = std::chrono::system_clock::now();
+		const auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+		std::cout << "4d ref full: " << elapsed << std::endl;
+	}
+
+	{
+		const auto start = std::chrono::system_clock::now();
+		for (auto i = 0; i < N; ++i)
+		{
+			const auto x = (i + 0.5) / N;
+			const auto y = (i * golden_ratio) - std::floor(i * golden_ratio);
+			const auto z = y;
+			const auto t = x;
+			volatile auto v = Voroce::Evaluate4DOpt(glm::vec4(x * 2048 - 1024, y * 2048 - 1024, z * 2048 - 1024, t * 2048 - 1024), Voroce::Hash4DLowQuality).first;
+		}
+		const auto end = std::chrono::system_clock::now();
+		const auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+		std::cout << "4d opt full: " << elapsed << std::endl;
+	}
+
+	{
+		// bug or numerical error
+		std::mt19937 mt(15);
+		std::uniform_real_distribution<double> uni(0.0f, 1.0f);
+		for (auto L = 0; L < 256; ++L)
 		{
 			std::cout << "4d: " << L << std::endl;
 			std::vector<std::tuple<double, double, double, double>> RND(T);
@@ -180,12 +323,36 @@ int main()
 			std::for_each(std::execution::par, RND.begin(), RND.end(), [&](std::tuple<double, double, double, double>& tuple)
 				{
 					const auto [x, y, z, t] = tuple;
-					const auto jitter = 1.0f;
-					const auto ref = Voroce::Evaluate4DRef(glm::vec4(x * 128 - 64, y * 128 - 64, z * 128 - 64, t * 128 - 64), Voroce::Hash4DLowQuality, jitter);
-					const auto opt = Voroce::Evaluate4DOpt(glm::vec4(x * 128 - 64, y * 128 - 64, z * 128 - 64, t * 128 - 64), Voroce::Hash4DLowQuality, jitter);
+					const auto ref = Voroce::Evaluate4DRef(glm::vec4(x * 2048 - 1024, y * 2048 - 1024, z * 2048 - 1024, t * 2048 - 1024), Voroce::Hash4DLowQuality, 0.5f);
+					const auto opt = Voroce::Evaluate4DOpt(glm::vec4(x * 2048 - 1024, y * 2048 - 1024, z * 2048 - 1024, t * 2048 - 1024), Voroce::Hash4DLowQuality, 0.5f);
 					if (ref.second != opt.second)
 					{
-						std::cout << "NG: ref:" << ref.first << " opt:" << opt.first << " ref:" << std::setprecision(16) << ref.second << " opt:" << opt.second << " jitter:" << jitter << std::endl;
+						std::cout << "4d: ref:" << ref.first << " opt:" << opt.first << " ref:" << std::setprecision(16) << ref.second << " opt:" << opt.second << std::endl;
+					}
+				});
+		}
+	}
+
+	{
+		// bug or numerical error
+		std::mt19937 mt(15);
+		std::uniform_real_distribution<double> uni(0.0f, 1.0f);
+		for (auto L = 0; L < 256; ++L)
+		{
+			std::cout << "4d: " << L << std::endl;
+			std::vector<std::tuple<double, double, double, double>> RND(T);
+			for (auto& r : RND)
+			{
+				r = { uni(mt), uni(mt), uni(mt), uni(mt) };
+			}
+			std::for_each(std::execution::par, RND.begin(), RND.end(), [&](std::tuple<double, double, double, double>& tuple)
+				{
+					const auto [x, y, z, t] = tuple;
+					const auto ref = Voroce::Evaluate4DRef(glm::vec4(x * 2048 - 1024, y * 2048 - 1024, z * 2048 - 1024, t * 2048 - 1024), Voroce::Hash4DLowQuality);
+					const auto opt = Voroce::Evaluate4DOpt(glm::vec4(x * 2048 - 1024, y * 2048 - 1024, z * 2048 - 1024, t * 2048 - 1024), Voroce::Hash4DLowQuality);
+					if (ref.second != opt.second)
+					{
+						std::cout << "4d: ref:" << ref.first << " opt:" << opt.first << " ref:" << std::setprecision(16) << ref.second << " opt:" << opt.second << std::endl;
 					}
 				});
 		}
